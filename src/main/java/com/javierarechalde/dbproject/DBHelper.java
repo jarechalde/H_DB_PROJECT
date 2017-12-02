1a86c3496708a93b6d90176496547a39c43a4ad4
Javier <jarechalde@gmail.com>
2017-12-02 14:26:53 -0500

    Fixed Unable to connect to the DS
    Now we connected Succesfully to the DB and introduced a record

--- src/main/java/com/javierarechalde/dbproject/DBHelper.java
+++ src/main/java/com/javierarechalde/dbproject/DBHelper.java
@@ -2,6 +2,7 @@ package com.javierarechalde.dbproject;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -12,59 +13,49 @@ import org.flywaydb.core.Flyway;
 
 public class DBHelper {
 
-	public static Connection getConnection() throws SQLException {
-		return getInstance().getDataSource().getConnection();
-	}
+  public static Connection getConnection() throws SQLException {
+    return DBHelper.getInstance().getDataSource().getConnection();
+  }
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(DBHelper.class);
-	
-	private static final DBHelper INSTANCE = new DBHelper();
-	
-	public static DBHelper getInstance() {
-		return DBHelper.INSTANCE;
-	}
-	
-	private BasicDataSource ds;
-	
-	private DBHelper() {		
-	}
-	
+  public static DBHelper getInstance() {
+    return DBHelper.INSTANCE;
+  }
 
-	
-	//Initialize
-	public void init() {
-		LOGGER.debug("Creating the datasource");
-		//Parameters to connect to the database
-		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName("org.h2.Driver");
-		ds.setUrl("jdbc:h2:target/db");
-		ds.setUsername("root");
-		ds.setPassword("16387495p");
-		
-		//Doing the migration before we use the data source
-		LOGGER.debug("Executing Flyway (database migration)");
-		Flyway flyway = new Flyway();
-		flyway.setDataSource(ds);
-		flyway.migrate();
-	}
-	
-	public DataSource getDataSource() {
-		return ds;
-	}
-	
-	//Close it
-	public void close() {
-		//Checking if we initialized before trying to close
-		if(ds != null) {
-			LOGGER.debug("Closing the data source");
-			try {
-				ds.close();
-			} catch (SQLException e) {
-				LOGGER.error("Failed to close the data source", e);
-			}
-			
-		} 
-		
-	}
-	
+  private static final Logger LOGGER = LoggerFactory.getLogger(DBHelper.class);
+
+  private static final DBHelper INSTANCE = new DBHelper();
+
+  private BasicDataSource ds;
+
+  private DBHelper() {}
+
+  public void close() {
+    if (ds != null) {
+      DBHelper.LOGGER.debug("Closing the data source");
+      try {
+        ds.close();
+      } catch (final SQLException e) {
+        DBHelper.LOGGER.error("Failed to close the data source", e);
+      }
+    }
+  }
+
+  public DataSource getDataSource() {
+    return ds;
+  }
+
+  public void init() {
+    DBHelper.LOGGER.debug("Creating the data source");
+    ds = new BasicDataSource();
+    ds.setDriverClassName("org.h2.Driver");
+    ds.setUrl("jdbc:h2:target/db");
+    ds.setUsername("jarechalde");
+    ds.setPassword("16387495p");
+
+    DBHelper.LOGGER.debug("Executing Flyway (database migration)");
+    final Flyway flyway = new Flyway();
+    flyway.setDataSource(ds);
+    flyway.migrate();
+  }
+
 }
